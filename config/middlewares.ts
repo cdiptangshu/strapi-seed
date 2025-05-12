994e1e3cbc77acb759a6da871fc5b19d64cffd9c
Diptangshu Chakrabarty <cdiptangshu@gmail.com>
2025-05-12 16:47:00 +0530

    bug: image preview not visible in production in admin ui
    Added CSP config to allow `https://res.cloudinary.com`
    
    Refs: #33

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,18 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'default-src': ["'self'"],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://res.cloudinary.com'],
+        }
+      }
+    }
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
